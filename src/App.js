1f3aefa4c4eb1197c5f853fcbf5a492a9122fddb
Minhazul Alam <78158511+akib-chy@users.noreply.github.com>
2022-04-08 15:42:23 +0600

    user login from make on firebase

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import { getAuth } from "firebase/auth";
+import app from "./firebase.init";
+import { Button, Form } from "react-bootstrap";
 
+const auth = getAuth(app);
 function App() {
+  const onEmailBlur = (event) => {
+    console.log(event.target.value);
+  };
+  const onPasseordBlur = (event) => {
+    console.log(event.target.value);
+  };
+  const handleFromSubmit = (event) => {
+    console.log("from submited done");
+    event.preventDefault();
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container w-50 mt-4 border p-4 rounded shadow">
+      <Form onSubmit={handleFromSubmit}>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control
+            onBlur={onEmailBlur}
+            type="email"
+            placeholder="Enter email"
+          />
+          <Form.Text className="text-muted">
+            We'll never share your email with anyone else.
+          </Form.Text>
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            onBlur={onPasseordBlur}
+            type="password"
+            placeholder="Password"
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicCheckbox">
+          <Form.Check type="checkbox" label="Check me out" />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
     </div>
   );
 }
---
+++ src/firebase.init.jsx
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyABPzRJS0R7-_PggJyt7swQT3hBolBuNmA",
+  authDomain: "simple-firebase-user-login.firebaseapp.com",
+  projectId: "simple-firebase-user-login",
+  storageBucket: "simple-firebase-user-login.appspot.com",
+  messagingSenderId: "874199515141",
+  appId: "1:874199515141:web:2b51800f64855f12f09c6c",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
